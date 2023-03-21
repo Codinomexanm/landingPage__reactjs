bd1e790d545beb5d7f282bd7661bff53eadbeba5
Alexandre <veladorisz@gmail.com>
2023-03-21 17:00:25 -0300

    adicionado o leaflet e configurações extras

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,23 +1,45 @@
 import React from 'react'
 import './Container.css'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+
 
 const Container = () => {
   return (
     <div className='container'>
-      <div className='xxt'><h1>deu 1</h1></div>
-    <div>
+      <div className='xxt'>
+        <h1>deu 1</h1>
+      </div>
+      <div>
         <section id="transform-world">
           <p>TRANSFORME O MUNDO COM A GENTE</p>
         </section>
-    </div>
-    <div className='xxt'><h1>deu 3</h1></div>
-    <div>
+      </div>
+      <div className='xxt'>
+        <h1>deu 3</h1>
+      </div>
+      <div>
         <section id="transform-world1">
           <p>TRANSFORME O MUNDO COM A GENTE</p>
         </section>
+      </div>
+      <div className='xxt xxt2'>
+        <div id="map">
+          <MapContainer className='left' center={[-15.774314204480694, -48.2823397892441]} zoom={15} scrollWheelZoom={false}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker className='pop'  position={[-15.774314204480694, -48.2823397892441]}>
+              
+              <Popup>
+                Igreja cátia
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
     </div>
-    </div>
-
   )
 }
 
