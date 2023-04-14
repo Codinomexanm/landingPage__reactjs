8162fd724866e411ac91e580002b606a852d99a0
Alexandre Pereira <veladorisz@gmail.com>
2023-04-14 17:46:13 -0300

    textos finais

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -29,13 +29,16 @@ const Container = () => {
         <div className='xs_content'>
           <h1>Queridos irmãos e irmãs em Cristo</h1>
           <p style={{ marginBottom: 50 }}>É com grande alegria que compartilhamos com vocês os eventos religiosos emocionantes que ocorrerão em nossa Igreja Evangélica "Vida Abundante" nas próximas semanas. Nós convidamos vocês a se juntarem a nós e participarem dessas oportunidades significativas de crescimento espiritual e comunhão.</p>
-          <ul>
+          <ul style={{ marginBottom: 50 }}>
             <li>Cultos de Adoração: Todos os domingos, às 10h e 18h, teremos nossos cultos de adoração na igreja. É um tempo especial de louvor, oração e ensino da Palavra de Deus. Nossos pastores e líderes estarão ministrando mensagens poderosas para fortalecer sua fé e ajudá-lo a crescer em seu relacionamento com Deus.</li>
             <li>Estudos Bíblicos: Durante a semana, teremos estudos bíblicos em pequenos grupos, onde você pode aprofundar seu conhecimento das Escrituras e compartilhar comunhão com outros crentes. Teremos estudos para diferentes faixas etárias, desde crianças até adultos, abordando diversos temas relevantes para a vida cristã.</li>
             <li>Programa de Jovens: Nossos jovens terão encontros especiais aos sábados à tarde, com atividades dinâmicas, louvor, compartilhamento de testemunhos e estudo bíblico. É uma oportunidade incrível para os jovens se conectarem, crescerem em sua fé e se envolverem em ministérios específicos para sua faixa etária.</li>
             <li>Eventos Especiais: Além dos cultos regulares e estudos bíblicos, teremos eventos especiais, como conferências, retiros espirituais, acampamentos e atividades de serviço comunitário. Esses eventos são momentos preciosos para se aprofundar em sua fé, se relacionar com outros membros da igreja e impactar a comunidade ao nosso redor.</li>
             <li>Ministérios de Ação Social: Nossa igreja também está envolvida em ministérios de ação social, como visitas a hospitais, orfanatos, lares de idosos e comunidades carentes. Se você tem um coração voltado para o serviço ao próximo, esses ministérios oferecem oportunidades significativas para compartilhar o amor de Deus através de ações práticas de compaixão e serviço.</li>
           </ul>
+          <p style={{ marginBottom: 50 }} >Estes são apenas alguns dos eventos religiosos que teremos em nossa Igreja Evangélica "Vida Abundante". Estamos animados em compartilhar essas oportunidades de crescimento espiritual e comunhão com vocês. Todos são bem-vindos para se juntar a nós em nossa jornada de fé e descobrir o amor incondicional de Deus. Venha participar e fazer parte de nossa comunidade de fé! Esperamos recebê-los de braços abertos.</p>
+          <strong>Deus abençoe,
+            Pastores e líderes da Igreja Evangélica "Vida Abundante"</strong>
         </div>
 
       </div>
@@ -43,10 +46,10 @@ const Container = () => {
         <section id="transform-world1">
         </section>
       </div>
-      <div className='xxt gals'>
+      <div className='gals'>
         <Gallery />
       </div>
-      <div className='xxt xxt2'>
+      <div className='xxt2'>
         <div id="map">
           <MapContainer className='left' center={[-15.774314204480694, -48.2823397892441]} zoom={15} scrollWheelZoom={false}>
             <TileLayer
@@ -61,7 +64,7 @@ const Container = () => {
           </MapContainer>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
